c5f0786890bf0c39742fa7a0333a60d5347a66b6
nothing010101 <mstrj719@gmail.com>
2026-05-14 15:34:14 +0700

    update: src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import Home from './pages/Home'
 import Feed from './pages/Feed'
 import Editorials from './pages/Editorials'
 import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
 
 export default function App() {
   return (
@@ -12,7 +11,6 @@ export default function App() {
       <Route path="/feed" element={<Feed />} />
       <Route path="/editorials" element={<Editorials />} />
       <Route path="/sign-in" element={<SignIn />} />
-      <Route path="/sign-up" element={<SignUp />} />
     </Routes>
   )
 }
